18d963da1897f6757533321df06fc8a757953b16
Omkar Kulkarni <kulkarni.omkar108@gmail.com>
2023-05-25 23:26:39 +0530

    Added socket.io initialisation

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,4 +2,6 @@ require("dotenv").config();
 
 export default {
   port: process.env.PORT || 8000,
+  webSocketOriginWhitelist:
+    process.env.WEBSOCKET_ORIGIN_WHITELIST?.split(",") || [],
 };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import config from "./config/config";
 
 const app: Application = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: config.webSocketOriginWhitelist,
+  },
+});
 
 app.get("/health", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({ success: "true" });
   next();
 });
-
-app.listen(config.port, () => {
-  console.log(`Server up and running on port ${config.port}.`);
+io.on("connection", (socket) => {
+  console.log(`WebSocket connection established with socket ID ${socket.id}.`);
 });
+
+server.listen(config.port);
+console.log(`Server listening on port ${config.port}.`);
