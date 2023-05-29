23e91648ffa9f1ff8a2d1d9667eb72fe178669b2
Omkar Kulkarni <kulkarni.omkar108@gmail.com>
2023-05-29 13:10:20 +0530

    Added message logging

--- src/services/webSocketService.ts
+++ src/services/webSocketService.ts
@@ -1,8 +1,16 @@
 import { Server, Socket } from "socket.io";
+import MessageService from "../services/messageService";
 
 class WebSocketService {
+  messageService: MessageService;
+
+  constructor() {
+    this.messageService = new MessageService();
+  }
+
   configureSendMessageEvent = (io: Server, socket: Socket) => {
-    socket.on("sendMessage", (senderPrivilege, content) => {
+    socket.on("sendMessage", async (senderPrivilege, content) => {
+      await this.messageService.createMessage(senderPrivilege, content);
       socket.broadcast.emit("receiveMessage", content);
     });
   };
